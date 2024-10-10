67c98fc4aba1c7d02c422d77a04aca4d741edc88
max-pw-fusion5 <max.pirotais-wilton@integration.works>
2024-10-11 11:40:15 +1300

    Added KeyVault Packages and implemented keyvault getting secrets

--- TradeMe_Test_WebApp_001/Helpers/OAuthHelper.cs
+++ TradeMe_Test_WebApp_001/Helpers/OAuthHelper.cs
@@ -2,6 +2,9 @@
 using System.Globalization;
 using System.Net;
 using System.Web;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
 using TradeMe_Test_WebApp_001.Models;
 
 namespace TradeMe_Test_WebApp_001.Helpers
@@ -17,6 +20,19 @@ namespace TradeMe_Test_WebApp_001.Helpers
         private static string temp_AccessTokenKeyName = string.Empty;
         private static string temp_AccessTokenSecretKeyName = string.Empty;
 
+        private static SecretClientOptions options = new SecretClientOptions()
+        {
+            Retry =
+            {
+                Delay= TimeSpan.FromSeconds(2),
+                MaxDelay = TimeSpan.FromSeconds(16),
+                MaxRetries = 5,
+                Mode = RetryMode.Exponential
+            }
+        };
+
+        private static SecretClient client = new SecretClient(new Uri("https://tmcreds-maxpw425.vault.azure.net/"), new DefaultAzureCredential(), options);
+
         //retrieve the time in seconds since epoch
         private static string GetOauthTimestamp()
         {
@@ -26,51 +42,11 @@ namespace TradeMe_Test_WebApp_001.Helpers
         public static string GetBaseOAuthHeader()
         {
             // "&" becomes %26 when Url encoded
-            var signature = HttpUtility.UrlEncode(OAuthHeaderData.ConsumerSecret + "&");
+            var signature = HttpUtility.UrlEncode(OAuthHelper.getSecret("ConsumerSecret") + "&");
             return string.Format(
                 "OAuth oauth_callback={0}, oauth_consumer_key={1}, oauth_version={2}, oauth_timestamp={3}, oauth_nonce={4}, oauth_signature_method={5}, oauth_signature={6}",
-                OAuthHeaderData.Callback, OAuthHeaderData.ConsumerKey, OAuthHeaderData.OAuthVersion, GetOauthTimestamp(), OAuthHeaderData.OAuthNonce, OAuthHeaderData.SignatureMethod, signature);
+                OAuthHeaderData.Callback, OAuthHelper.getSecret("ConsumerKey"), OAuthHeaderData.OAuthVersion, GetOauthTimestamp(), OAuthHeaderData.OAuthNonce, OAuthHeaderData.SignatureMethod, signature);
         }
-
-        //public static string MakeHttpRequest(string method, Uri url, string header)
-        //{
-
-        //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-        //    request.Method = method;
-        //    //we don't have a post body for any of the examples so must manually set the content length to 0
-        //    //this is for example only, if you were going to be making post requests with a body then this won't work
-        //    request.ContentLength = 0;
-
-        //    request.Headers.Add("Authorization", header);
-        //    var responseText = "Unknown response text";
-        //    try
-        //    {
-        //        var response = (HttpWebResponse)request.GetResponse();
-        //        var stream = response.GetResponseStream();
-        //        if (stream != null)
-        //            using (var reader = new StreamReader(stream))
-        //            {
-        //                responseText = reader.ReadToEnd();
-        //                response.Dispose();
-        //                reader.Dispose();
-        //            }
-        //    }
-        //    catch (WebException e)
-        //    {
-        //        var resp = new StreamReader(e.Response.GetResponseStream()).ReadToEnd();
-
-        //        //catch any errors and display in the debug window
-        //        if (e.Status == WebExceptionStatus.ProtocolError)
-        //        {
-        //            Debug.WriteLine("Status Code string : {0}", ((HttpWebResponse)e.Response).StatusCode);
-        //            Debug.WriteLine("Status Code number : {0:D}", ((HttpWebResponse)e.Response).StatusCode);
-        //            Debug.WriteLine("Status Description : {0}", ((HttpWebResponse)e.Response).StatusDescription);
-        //            Debug.WriteLine("Error Message : {0}", resp);
-        //            throw new WebException(resp, e);
-        //        }
-        //    }
-        //    return responseText;
-        //}
 
         public static bool setSecret(string secretName, string secretValue)
         {
@@ -89,7 +65,8 @@ namespace TradeMe_Test_WebApp_001.Helpers
                     temp_AccessTokenSecretKeyName = secretValue;
                     return true;
                 default:
-                    return false;
+                    client.SetSecret(secretName, secretValue);
+                    return true;
             }
         }
 
@@ -106,7 +83,10 @@ namespace TradeMe_Test_WebApp_001.Helpers
                 case "temp_AccessTokenSecretKeyName":
                     return temp_AccessTokenSecretKeyName;
                 default:
-                    return string.Empty;
+                    string secretContent = string.Empty;
+                    var secret = client.GetSecret(secretName).Value;
+                    secretContent = secret.Value;
+                    return secretContent;
             }
         }
 
